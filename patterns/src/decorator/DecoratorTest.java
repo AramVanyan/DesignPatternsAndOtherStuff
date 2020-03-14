7a2c0880b8e905e3655016aa5c7125acb0a37398
Aram Vanyan <aram.vanyan.a@gmail.com>
2020-03-14 22:15:50 +0400

    add decorator pattern

---
+++ patterns/src/decorator/DecoratorTest.java
@@ -0,0 +1,19 @@
+package decorator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DecoratorTest {
+    @Test
+    public void whenDecoratorsInjectedAtRuntime_thenConfigSuccess() {
+        ChristmasTree tree1 = new Garland(new ChristmasTreeImpl());
+        assertEquals(tree1.decorate(),
+                "Christmas tree with Garland");
+
+        ChristmasTree tree2 = new BubbleLights(
+                new Garland(new Garland(new ChristmasTreeImpl())));
+        assertEquals(tree2.decorate(),
+                "Christmas tree with Garland with Garland with Bubble Lights");
+    }
+}
